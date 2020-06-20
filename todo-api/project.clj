b5315795677c528792606662ee8e4313b754c06b
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2020-06-20 21:53:54 +0900

    Update dependencies
    - Add fipp, kawk

--- todo-api/project.clj
+++ todo-api/project.clj
@@ -1,14 +1,14 @@
 (defproject todo-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Todo API"
+  :url "https://github.com/lagenorhynque/clj-rest-api-and-cljs-spa-example"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [duct/core "0.7.0"]
+  :dependencies [[duct/core "0.8.0"]
                  [duct/module.ataraxy "0.3.0"]
-                 [duct/module.logging "0.4.0"]
-                 [duct/module.sql "0.5.0"]
+                 [duct/module.logging "0.5.0"]
+                 [duct/module.sql "0.6.0"]
                  [duct/module.web "0.7.0"]
-                 [org.postgresql/postgresql "42.2.5"]]
+                 [org.clojure/clojure "1.10.1"]
+                 [org.postgresql/postgresql "42.2.14"]]
   :plugins [[duct/lein-duct "0.12.1"]]
   :main ^:skip-aot todo-api.main
   :resource-paths ["resources" "target/resources"]
@@ -22,6 +22,8 @@
    :profiles/dev {}
    :project/dev  {:source-paths   ["dev/src"]
                   :resource-paths ["dev/resources"]
-                  :dependencies   [[integrant/repl "0.3.1"]
-                                   [eftest "0.5.7"]
-                                   [kerodon "0.9.0"]]}})
+                  :dependencies   [[eftest "0.5.9"]
+                                   [fipp "0.6.23"]
+                                   [hawk "0.2.11"]
+                                   [integrant/repl "0.3.1"]
+                                   [kerodon "0.9.1"]]}})
